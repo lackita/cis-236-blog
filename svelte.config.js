d2a8503cb103c3f60d399a6d87a8211c0341fa23
Colin Williams <lackita@gmail.com>
2024-01-22 22:47:53 -0500

    Github actions/pages isn't working particularly well, trying hobby tier of vercel instead

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from 'svelte-adapter-github';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
