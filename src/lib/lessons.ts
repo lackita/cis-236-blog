8b420f7f925ef82d8d19b5b7d4afbbfd7781d4a7
Colin Williams <lackita@gmail.com>
2024-01-28 08:03:37 -0500

    Migrate to Data Structure
    As the materials get more sprawling I decided to move the information
    about weeks to a central data structure. This will allow me to ensure
    the same value is used for a link as the route serving that link in a
    future change.

---
+++ src/lib/lessons.ts
@@ -0,0 +1,204 @@
+interface Lecture {
+  date: Date;
+  end: Date;
+  title: string;
+  path_name?: string;
+}
+
+interface Assignment {
+  title: string;
+  path_name?: string;
+  due: Date;
+  revisions?: Date;
+}
+
+interface Week {
+  lecture: Lecture;
+  assignment?: Assignment;
+}
+
+const calendar: Week[] = [
+  {
+    lecture: {
+      date: new Date(2024, 0, 23),
+      title: "Agile Project Structure",
+      path_name: "agile-project-structure",
+    },
+    assignment: {
+      title: "Site Research",
+      path_name: "site-research",
+      due: new Date(2024, 1, 1),
+    },
+  },
+  {
+    lecture: {
+      date: new Date(2024, 1, 1),
+      title: "Meet With Client",
+    },
+    assignment: {
+      title: "Story Creation",
+      due: new Date(2024, 1, 6),
+      revisions: new Date(2024, 1, 9),
+    },
+  },
+
+  {
+    lecture: {
+      date: new Date(2024, 1, 6),
+      title: "Estimation and Planning",
+    },
+    assignment: {
+      title: "Project Plan",
+      due: new Date(2024, 1, 13),
+      revisions: new Date(2024, 1, 16),
+    },
+  },
+
+  {
+    lecture: {
+      date: new Date(2024, 1, 13),
+      title: "Web Frameworks",
+    },
+    assignment: {
+      title: "Svelte and SvelteKit",
+      due: new Date(2024, 1, 20),
+      revisions: new Date(2024, 1, 23),
+    },
+  },
+
+  {
+    lecture: {
+      date: new Date(2024, 1, 20),
+      title: "User Experience",
+    },
+    assignment: {
+      title: "Static Content and User Profiles",
+      due: new Date(2024, 1, 27),
+      revisions: new Date(2024, 2, 1),
+    },
+  },
+
+  {
+    lecture: {
+      date: new Date(2024, 1, 27),
+      title: "Demo Day Prep",
+    },
+    assignment: {
+      title: "Demo Preparation",
+      due: new Date(2024, 2, 5),
+    },
+  },
+
+  {
+    lecture: {
+      date: new Date(2024, 2, 5),
+      title: "Demo Day",
+    },
+    assignment: {
+      title: "Feedback Summary",
+      due: new Date(2024, 2, 8),
+    },
+  },
+
+  {
+    lecture: {
+      date: new Date(2024, 2, 10),
+      end: new Date(2024, 2, 17),
+      title: "Spring Break",
+    },
+  },
+
+  {
+    lecture: {
+      date: new Date(2024, 2, 19),
+      title: "Databases",
+    },
+    assignment: {
+      title: "Schema Design",
+      due: new Date(2024, 2, 26),
+      revisions: new Date(2024, 2, 29),
+    },
+  },
+
+  {
+    lecture: {
+      date: new Date(2024, 2, 26),
+      title: "Shopping Carts",
+    },
+    assignment: {
+      title: "Shopping cart and item pages",
+      due: new Date(2024, 3, 2),
+      revisions: new Date(2024, 3, 5),
+    },
+  },
+
+  {
+    lecture: {
+      date: new Date(2024, 3, 2),
+      title: "Authentication",
+    },
+    assignment: {
+      title: "Login pages and cart conversion",
+      due: new Date(2024, 3, 9),
+      revisions: new Date(2024, 3, 12),
+    },
+  },
+
+  {
+    lecture: {
+      date: new Date(2024, 3, 9),
+      title: "Payments",
+    },
+    assignment: {
+      title: "Checkout",
+      due: new Date(2024, 3, 16),
+      revisions: new Date(2024, 3, 19),
+    },
+  },
+
+  {
+    lecture: {
+      date: new Date(2024, 3, 16),
+      title: "Monitoring",
+    },
+    assignment: {
+      title: "North star metric",
+      due: new Date(2024, 3, 23),
+      revisions: new Date(2024, 3, 26),
+    },
+  },
+
+  {
+    lecture: {
+      date: new Date(2024, 3, 23),
+      title: "Backend operations",
+    },
+    assignment: {
+      title: "Route planner",
+      due: new Date(2024, 3, 30),
+      revisions: new Date(2024, 4, 3),
+    },
+  },
+
+  {
+    lecture: {
+      date: new Date(2024, 3, 30),
+      title: "Prepare for final demo",
+    },
+    assignment: {
+      title: "Prep for final demo",
+      due: new Date(2024, 4, 7),
+      revisions: new Date(2024, 4, 7),
+    },
+  },
+
+  {
+    lecture: {
+      date: new Date(2024, 4, 7),
+      title: "Final Demo",
+    },
+  },
+];
+
+export type { Week };
+export { calendar };
--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -1,3 +1,18 @@
+<script lang="ts">
+  import { calendar } from "$lib/lessons";
+  import { onMount } from "svelte";
+
+  let language: string;
+  onMount(() => (language = navigator.language));
+  function format_date(date: Date): string {
+    return date.toLocaleDateString(language, {
+      weekday: "short",
+      month: "short",
+      day: "numeric",
+    });
+  }
+</script>
+
 <h1>Calendar</h1>
 
 <p>
@@ -22,129 +37,42 @@
     <th>Final Revision</th>
   </tr>
 
-  <tr>
-    <td>Jan 23</td>
-    <td>
-      <a href="/lectures/agile-project-structure">Agile Project Structure</a>
-    </td>
-    <td>
-      <a href="/assignments/site-research">Site Research</a>
-    </td>
-    <td>Feb 1</td>
-    <td>No revisions</td>
-  </tr>
-
-  <tr>
-    <td>Feb 1</td>
-    <td>Meet With Client</td>
-    <td>Story Creation</td>
-    <td>Feb 6</td>
-    <td>Feb 9</td>
-  </tr>
-
-  <tr>
-    <td>Feb 6</td>
-    <td>Estimation and Planning</td>
-    <td>Project Plan</td>
-    <td>Feb 13</td>
-    <td>Feb 16</td>
-  </tr>
-
-  <tr>
-    <td>Feb 13</td>
-    <td>Web Frameworks</td>
-    <td>Svelte and SvelteKit</td>
-    <td>Feb 20</td>
-    <td>Feb 23</td>
-  </tr>
-
-  <tr>
-    <td>Feb 20</td>
-    <td>User Experience</td>
-    <td>Static Content and User Profiles</td>
-    <td>Feb 27</td>
-    <td>Mar 1</td>
-  </tr>
-
-  <tr>
-    <td>Feb 27</td>
-    <td>Demo Day Prep</td>
-    <td>Demo Preparation</td>
-    <td>Mar 5</td>
-    <td>No Revisions</td>
-  </tr>
-
-  <tr>
-    <td>Mar 5</td>
-    <td>Demo Day</td>
-    <td>Feedback Summary</td>
-    <td>Mar 8</td>
-    <td>No Revisions</td>
-  </tr>
-
-  <tr>
-    <td>Mar 10-17</td>
-    <td>Spring Break</td>
-  </tr>
-
-  <tr>
-    <td>Mar 19</td>
-    <td>Databases</td>
-    <td>Schema Design</td>
-    <td>Mar 26</td>
-    <td>Mar 29</td>
-  </tr>
-
-  <tr>
-    <td>Mar 26</td>
-    <td>Shopping Carts</td>
-    <td>Shopping cart and item pages</td>
-    <td>Apr 2</td>
-    <td>Apr 5</td>
-  </tr>
-
-  <tr>
-    <td>Apr 2</td>
-    <td>Authentication</td>
-    <td>Login pages and cart conversion</td>
-    <td>Apr 9</td>
-    <td>Apr 12</td>
-  </tr>
-
-  <tr>
-    <td>Apr 9</td>
-    <td>Payments</td>
-    <td>Checkout</td>
-    <td>Apr 16</td>
-    <td>Apr 19</td>
-  </tr>
-
-  <tr>
-    <td>Apr 16</td>
-    <td>Monitoring</td>
-    <td>North star metric</td>
-    <td>Apr 23</td>
-    <td>Apr 26</td>
-  </tr>
-
-  <tr>
-    <td>Apr 23</td>
-    <td>Backend operations</td>
-    <td>Route planner</td>
-    <td>Apr 30</td>
-    <td>May 3</td>
-  </tr>
-
-  <tr>
-    <td>Apr 30</td>
-    <td>Prepare for final demo</td>
-    <td>Prep for final demo</td>
-    <td>May 7</td>
-    <td>May 7</td>
-  </tr>
-
-  <tr>
-    <td>May 7</td>
-    <td>Final Demo</td>
-  </tr>
+  {#each calendar as { lecture, assignment }}
+    <tr>
+      <td>
+        {format_date(lecture.date)}
+        {#if lecture.end}
+          to {format_date(lecture.end)}
+        {/if}
+      </td>
+      <td>
+        {#if lecture.path_name}
+          <a href="/lectures/{lecture.path_name}">{lecture.title}</a>
+        {:else}
+          {lecture.title}
+        {/if}
+      </td>
+      {#if assignment}
+        <td>
+          {#if assignment.path_name}
+            <a href="/assignments/{assignment.path_name}">{assignment.title}</a>
+          {:else}
+            {assignment.title}
+          {/if}
+        </td>
+        <td>{format_date(assignment.due)}</td>
+        <td>
+          {assignment.revisions
+            ? format_date(assignment.revisions)
+            : "No Revisions"}
+        </td>
+      {/if}
+    </tr>
+  {/each}
 </table>
+
+<style>
+  table {
+    width: 100%;
+  }
+</style>
